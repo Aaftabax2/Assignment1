e11e3ce90b77f143846f4a8bb2e33a70aac78533
Aaftab Ahmad <the.stat.empires@gmail.com>
2023-08-07 00:36:44 +0500

    Rename files to Question

---
+++ Question 2.js
@@ -0,0 +1,4 @@
+"use strict";
+let myName = "Aaftab Ahmad";
+let msg = "Hello " + myName + " would you like to learn some Python today?";
+console.log(msg);
---
+++ Question 2.ts
@@ -0,0 +1,3 @@
+let myName:string="Aaftab Ahmad";
+let msg: string="Hello "+ myName+" would you like to learn some Python today?";
+console.log(msg)
